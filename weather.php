feda383e892e7690c5af0a25ff9a24491147bb90
Noey <pakaprn.t@linecorp.com>
2019-07-01 00:27:14 +0700

    Push message when raining

--- weather.php
+++ weather.php
@@ -13,32 +13,30 @@ curl_close($ch);
 
 $obj = json_decode($result, true);
 if ($obj[0]["HasPrecipitation"]) {
-  echo "raining";
-} else {
-  echo "no rain";
+  $weather_link = $obj[0]["MobileLink"];
+  $message = "It's raining now\n". $weather_link;
+  pushMessage($message);
 }
 
-// Push message
-/*
-$access_token = 'tWEShrbn4QrklPwjlObTVPhhGo5AnJAL/YAkZB0OaaC1rLhITnIfRDNq3s0/pTyiBAwkj6ysNYk45abbUh/hBHvi+JC0GCME7kHXnM2J8lLhLC2sE3eiMlMzObRa0fNmiWvpuFd34l8nS6Mw6Xo7cwdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v2/bot/message/push';
+function pushMessage($message) {
+  $access_token = 'tWEShrbn4QrklPwjlObTVPhhGo5AnJAL/YAkZB0OaaC1rLhITnIfRDNq3s0/pTyiBAwkj6ysNYk45abbUh/hBHvi+JC0GCME7kHXnM2J8lLhLC2sE3eiMlMzObRa0fNmiWvpuFd34l8nS6Mw6Xo7cwdB04t89/1O/w1cDnyilFU=';
+  $url = 'https://api.line.me/v2/bot/message/push';
 
-$headers = array('Authorization: Bearer ' . $access_token, 'Content-Type: application/json');
-$data = '{"to": "U7cbafaedd599e8edd822e5e15476ddf8",';
-$data .= '"messages":[';
-$data .= '{"type":"text",';
-$data .= '"text":"hello"}]}';
+  $headers = array('Authorization: Bearer ' . $access_token, 'Content-Type: application/json');
+  $data = '{"to": "U7cbafaedd599e8edd822e5e15476ddf8",';
+  $data .= '"messages":[';
+  $data .= '{"type":"text",';
+  $data .= '"text":"'.$message.'"}]}';
 
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-$result = curl_exec($ch);
-curl_close($ch);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  $result = curl_exec($ch);
+  curl_close($ch);
+}
 
-echo $result;
-*/
 ?>
