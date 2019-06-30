b0ca7f2f09a17bbb1fa0d0bbe6ffa7b6fc107232
Noey <pakaprn.t@linecorp.com>
2019-07-01 00:46:35 +0700

    Update weather.php

--- weather.php
+++ weather.php
@@ -19,19 +19,18 @@ if ($obj[0]["HasPrecipitation"]) {
 }
 else {
   $weather_link = $obj[0]["MobileLink"];
-  $message = "No rain --- ". $weather_link;
-  pushMessage($message);
+  pushMessage( getFlexMessage($weather_link) );
 }
 
-function pushMessage($message) {
+function pushMessage($flex_message) {
   $access_token = 'tWEShrbn4QrklPwjlObTVPhhGo5AnJAL/YAkZB0OaaC1rLhITnIfRDNq3s0/pTyiBAwkj6ysNYk45abbUh/hBHvi+JC0GCME7kHXnM2J8lLhLC2sE3eiMlMzObRa0fNmiWvpuFd34l8nS6Mw6Xo7cwdB04t89/1O/w1cDnyilFU=';
   $url = 'https://api.line.me/v2/bot/message/push';
 
   $headers = array('Authorization: Bearer ' . $access_token, 'Content-Type: application/json');
   $data = '{"to": "U7cbafaedd599e8edd822e5e15476ddf8",';
   $data .= '"messages":[';
-  $data .= '{"type":"text",';
-  $data .= '"text":"'.$message.'"}]}';
+  $data .= '{"type":"flex",';
+  $data .= '"contents":'.$flex_message.'}]}';
 
 
   $ch = curl_init();
@@ -42,6 +41,95 @@ function pushMessage($message) {
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
   $result = curl_exec($ch);
   curl_close($ch);
+}
+
+function getFlexMessage($link_url){
+  return '
+  {
+    "type": "bubble",
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "spacing": "md",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "baseline",
+          "spacing": "md",
+          "contents": [
+            {
+              "type": "text",
+              "text": "Weather notification",
+              "weight": "bold",
+              "size": "xl",
+              "flex": 0
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "spacing": "md",
+          "margin": "xl",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "flex": 0,
+              "contents": [
+                {
+                  "type": "image",
+                  "url": "https://developer.accuweather.com/sites/default/files/12-s.png",
+                  "aspectRatio": "1:1",
+                  "size": "sm",
+                  "gravity": "bottom"
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "flex": 1,
+              "spacing": "xs",
+              "contents": [
+                {
+                  "type": "spacer",
+                  "size": "sm"
+                },
+                {
+                  "type": "text",
+                  "text": "It is raining now",
+                  "color": "#111111",
+                  "wrap": true,
+                  "gravity": "top",
+                  "size": "md"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "footer": {
+      "type": "box",
+      "layout": "vertical",
+      "spacing": "md",
+      "margin": "xl",
+      "contents": [
+        {
+          "type": "button",
+          "style": "primary",
+          "color": "#00B900",
+          "action": {
+            "type": "uri",
+            "label": "See details",
+            "uri": "' . $link_url .'"
+          }
+        }
+      ]
+    }
+  }
+  ';
 }
 
 ?>
