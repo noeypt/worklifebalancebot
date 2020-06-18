802f038943e209797f15924c6366f2fa709c4771
Noey <pakaprn.t@linecorp.com>
2020-06-18 13:56:38 +0700

    Update weather.php

--- weather.php
+++ weather.php
@@ -49,7 +49,7 @@ function getWeatherUrlIfRain($location_key){
     return $obj[0]["MobileLink"];
   }
   //else return null;
-  else return $obj[0]["MobileLink"]
+  else return $obj[0]["MobileLink"]ว
 }
 
 // Map location and user
